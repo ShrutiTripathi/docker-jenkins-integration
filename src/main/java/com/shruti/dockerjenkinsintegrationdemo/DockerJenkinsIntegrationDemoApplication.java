598f2b95f1f16b19872a12975edc419195f683bf
ShrutiTripathi <shrutitripathi1019@gmail.com>
2022-04-01 10:20:15 -0700

    first commit

---
+++ src/main/java/com/shruti/dockerjenkinsintegrationdemo/DockerJenkinsIntegrationDemoApplication.java
@@ -0,0 +1,20 @@
+package com.shruti.dockerjenkinsintegrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsIntegrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsIntegrationDemoApplication.class, args);
+	}
+
+}
+//echo "# docker-jenkins-integration-demo" >> README.md
+//		git init
+//		git add README.md
+//		git commit -m "first commit"
+//		git branch -M main
+//		git remote add origin https://github.com/ShrutiTripathi/docker-jenkins-integration-demo.git
+//		git push -u origin main
---
+++ src/test/java/com/shruti/dockerjenkinsintegrationdemo/DockerJenkinsIntegrationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shruti.dockerjenkinsintegrationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
