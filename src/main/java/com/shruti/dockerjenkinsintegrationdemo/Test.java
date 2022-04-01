7101e7a657aeb1832d1d21ea31ffeb1e57938cf2
ShrutiTripathi <shrutitripathi1019@gmail.com>
2022-04-01 14:09:00 -0700

    fifth commit

--- src/main/java/com/shruti/dockerjenkinsintegrationdemo/Test.java
+++ src/main/java/com/shruti/dockerjenkinsintegrationdemo/Test.java
@@ -2,6 +2,6 @@ package com.shruti.dockerjenkinsintegrationdemo;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("configured git with jenkins");
+        System.out.println("configured git with jenkins!!....");
     }
 }
